aef1424c47894284c04f52270d809033b8f8af59
RakhaRandika <rakharandikaaa6@gmail.com>
2026-02-03 15:06:59 +0800

    update contact dan hapus icon location

--- resources/views/pages/kontak.blade.php
+++ resources/views/pages/kontak.blade.php
@@ -15,7 +15,7 @@
 
 <!-- Contact Section -->
 <div class="container mx-auto px-4 py-16">
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
         <!-- Contact Info -->
         <div>
             @include('components.kontak.contact-info')
